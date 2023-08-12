95cdf41438e82c896a9eeac48aa2d2c7a9733bb3
空欠- <114565624+natureskybase@users.noreply.github.com>
2023-08-13 02:26:54 +0800

    gou tou

--- progressbar.py
+++
@@ -1,8 +0,0 @@
-import time
-
-size = 0
-content_size = 100
-for i in range(10):
-    size += 10
-    time.sleep(1)
-    print('\r', '[下载进度]:%s%.2f%%' % ('>'*int(size/10), float(size/content_size *100)), end=' ')
